1e7706318e7a72e911b1a8604d5c9a03e2dec292
Eiyuulia <okkyprasetian@gmail.com>
2021-05-25 12:04:40 +0700

    lol

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,12 +1,30 @@
 // tangkap variabel
+const simple = document.getElementById('simple');
+const hex = document.getElementById('hex');
+const body = document.getElementsByTagName('body')[0];
+const button = document.querySelector('#button');
 
-const simple = document.getElementById(simple);
-const hex = document.getElementById(hex);
+// function 
 
-function nav(){
-    simple.addEventListener("onclick", function => {
-        
-    })
+function clickMe(){
+    let color = 'red';
+    let curColor = body.style.backgroundColor;
+
+    switch(curColor){
+        case 'red':
+            color = 'blue';
+            break;
+        case 'blue':
+            color = 'green';
+            break;
+        case 'green':
+            color = 'red';
+            break;
+    }
+
+    curColor = color;
 }
 
-nav();  
+// event listener
+
+button.addEventListener('onclick', clickMe())
