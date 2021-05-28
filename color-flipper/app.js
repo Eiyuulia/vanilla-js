187de169b844e9e5bced9bbdf8f43cf2c65591bf
Eiyuulia <okkyprasetian@gmail.com>
2021-05-28 08:42:26 +0700

    tambah

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -3,28 +3,22 @@ const simple = document.getElementById('simple');
 const hex = document.getElementById('hex');
 const body = document.getElementsByTagName('body')[0];
 const button = document.querySelector('#button');
+const color = document.getElementById('color')
+
+// variabel yg diperlukan
+const colors = ["red", "green", "blue"];
+let iterasi = 0;
 
 // function 
 
-function clickMe(){
-    let color = 'red';
-    let curColor = body.style.backgroundColor;
-
-    switch(curColor){
-        case 'red':
-            color = 'blue';
-            break;
-        case 'blue':
-            color = 'green';
-            break;
-        case 'green':
-            color = 'red';
-            break;
-    }
-
-    curColor = color;
-}
-
 // event listener
 
-button.addEventListener('onclick', clickMe())
+button.addEventListener('click', function(){
+    iterasi++;
+    if(iterasi == 3){
+        iterasi = 0;
+    }
+
+    body.style.backgroundColor = colors[iterasi];
+    color.textContent = colors[iterasi];
+})
