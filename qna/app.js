ad0f288306df188548050a2097339c57e47bcbfb
Eiyuulia <okkyprasetian@gmail.com>
2021-09-05 10:39:40 +0700

    add

--- qna/app.js
+++ qna/app.js
@@ -1,26 +1,23 @@
-// tangkap 
+// Catch
 const imgs = document.querySelectorAll(".img");
 const sections = document.getElementsByTagName("section");
 
+// Create Element
 
-// var
-let arrp = [];
+let texts = []; 
 
-let arrt = [];
-arrt[0] = document.createTextNode("paragraph 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt architecto molestiae fugit libero quod perferendis totam, quas numquam recusandae cumque rem obcaecati consequuntur quis quisquam incidunt neque doloribus ducimus.");
-arrt[1] = document.createTextNode("paragraph 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt architecto molestiae fugit libero quod perferendis totam, quas numquam recusandae cumque rem obcaecati consequuntur quis quisquam incidunt neque doloribus ducimus.");
-arrt[2] = document.createTextNode("paragraph 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt architecto molestiae fugit libero quod perferendis totam, quas numquam recusandae cumque rem obcaecati consequuntur quis quisquam incidunt neque doloribus ducimus.");
-
-
-for(let i = 0; i < 3; i++){
-    arrp[i] = document.createElement("p");
-    arrp[i].appendChild(arrt[i]);
+for (let i = 0; i<3; i++){
+    texts[i] = document.createElement('div');
+    texts[i].innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magni error ex itaque eum facere iste quas, quia nobis, laborum fugiat quibusdam necessitatibus praesentium. Temporibus laudantium magni sunt, praesentium voluptatem, pariatur necessitatibus dicta officia iusto molestias perferendis voluptas alias, aliquam totam. Quisquam vero quis quibusdam itaque odio atque nihil rerum.';
+    texts[i].style.cssText = 'margin-top: 20px; padding-right: 20px; font-size: 12px; font-weight: bold; text-align: justify;';
+    texts[i].classList = 'noneDisplay';
+    sections[i].appendChild(texts[i]);
 }
 
 
-// event listener
-for(let i = 0; i < imgs.length; i++){
-    imgs[i].addEventListener("click", function(){
-        sections[i].appendChild(arrp[i]);
+// Function
+for (let i = 0; i<3; i++){
+    imgs[i].addEventListener('click', button =>{
+        texts[i].classList.toggle('noneDisplay');
     })
 }
